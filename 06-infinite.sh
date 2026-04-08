8ba4b061eb6a97611bd38823a780277c9bdc7499
HugoPadilla1 <hpadilla@ggc.edu>
2026-04-08 16:15:28 -0400

    Changed 06-Infinite

--- 06-infinite.sh
+++ 06-infinite.sh
@@ -3,10 +3,23 @@
 # the if statement had, if-then-fi
 # while loop has, while-do-done
 # true and false are also Unix commands
-while [ true ]; do
-  echo "infinite number of beer on the wall"
-done
+# while [ true ]; do
+#   echo "infinite number of beer on the wall"
+# done
 
 # exercise: write a script that continues to look up
 # the ip address of a given hostname (using nslookup) until
 # the user decides to stop
+
+while true; do
+  echo "Enter a hostname to look up (or type 'exit' to quit):"
+  read hostname
+
+  if [ "$hostname" = "exit" ]; then
+    echo "Exiting script."
+    break
+  fi
+
+  nslookup "$hostname"
+  echo ""
+done
