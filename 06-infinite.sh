c1c4030a21366d5e1379149535dcc17de700dfba
HugoPadilla1 <hpadilla@ggc.edu>
2026-04-08 16:42:23 -0400

    updated infinite

--- 06-infinite.sh
+++ 06-infinite.sh
@@ -22,8 +22,4 @@ while true; do
 
   nslookup "$hostname"
   echo ""
-<<<<<<< HEAD
-=======
-  echo "This is different for the second Assignment."
->>>>>>> 0791a95d7073284626ad7b40ce803e80075c6f07
 done
